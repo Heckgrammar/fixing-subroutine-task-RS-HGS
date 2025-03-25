7d5466c5b5895deda7fdf61ff5213e30a8241ebb
RS-HGS <144434850+RS-HGS@users.noreply.github.com>
2025-03-25 10:08:15 +0000

    Update Program.cs

--- FixSubroutineTask/Program.cs
+++ FixSubroutineTask/Program.cs
@@ -1,17 +1,18 @@
-﻿namespace FixSubroutineTask
+using System;﻿
+namespace FixSubroutineTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            for(int i = 8;i >= 0; i++) {
+            for(int i = 8 ;i > 0; i--) {
                 Console.WriteLine(diffCurrencies(i));            
             }
         }
         static string diffCurrencies(int x)
         {
             string[] currencies = { "baht", "dollar", "euro", "koruna", "lira", "rand", "rupee", "yen" };
-            return currencies[1];
+            return currencies[x-1];
 
         }
     }
